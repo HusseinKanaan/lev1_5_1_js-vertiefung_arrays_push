e5fdd78f7c5ca54f6e07e429591abc755fda489a
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-08-09 01:11:49 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+let songs = [
+    'Bohemian Rhapsody',
+    'Stairway to Heaven',
+    'Hotel California'
+];
+console.log(songs);
+
+let totalSongs = songs.push('Sweet Child O Mine', 'Hey jude', 'smells Like Teen Spirit');
+console.log(totalSongs)
+
+
+
+let besteFussballerAllerZeiten = [
+    ['Linonel Messi',
+    'Pele',
+    'Diego Armando Maradona',
+    'Christiano Ronaldo',
+    'Zinedine Zidane',
+    'Gianluigi Buffon',
+    'Oliver Kahn',
+    'Iker Casillas'
+]
+]
+console.log(besteFussballerAllerZeiten)
+
+
+const artists = ["michael",
+ "elvis"
+];
+
+artists.push("justin",
+ "charlie"
+ );
+
+console.log(artists)
